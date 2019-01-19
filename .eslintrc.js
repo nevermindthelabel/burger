52d5ce867345fdb664409c773348101b9b73d4cd
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-19 07:29:05 -0700

    initial file setup

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "extends": [
+      "airbnb-base",
+      "prettier"
+  ],
+  "rules": {
+      "no-console": 0,
+      "no-use-before-define": 0,
+      "no-plusplus": 0,
+      "no-undef": 0,
+  },
+  "env": {
+      "node": true,
+      "browser": true
+  },
+  "plugins": [
+      "prettier",
+  ]
+}
