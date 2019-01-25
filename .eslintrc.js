aadc92486354224a7f166f5924a95cf21a340afd
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-25 07:33:41 -0700

    updated eslint to include prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,8 @@
 module.exports = {
-  "extends": [
-      "airbnb-base",
-      "prettier"
-  ],
-  "rules": {
-      "no-console": 0,
-      "no-use-before-define": 0,
-      "no-plusplus": 0,
-      "no-undef": 0,
+  extends: ["airbnb", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": ["error"],
+    "no-console": 0
   },
-  "env": {
-      "node": true,
-      "browser": true
-  },
-  "plugins": [
-      "prettier",
-  ]
-}
+};
