e21ebe8f332fa367905469fb592437aa8bcc39ec
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-25 07:40:07 -0700

    added burger object

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,15 @@
-const orm = require('../config/orm')
+const orm = require('../config/orm');
+
+const burger = {
+  all: callback => orm.selectAll('burgers', response => callback(response)),
+
+  createOne: (values, callback) => {
+    orm.insertOne('burgers', values, response => callback(response));
+  },
+
+  updateOne: (colName, id, callback) => {
+    orm.updateOne('burgers', [colName, id], response => callback(response));
+  }
+};
+
+module.exports = burger;
