67cf588a4defa3c1249f93a2a94d4ca548edbd5c
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-27 14:40:40 -0700

    added jQuery to handle onclick events ajax requests

---
+++ public/assets/js/burger.js
@@ -0,0 +1,35 @@
+$(() => {
+  // create new burger
+  $('.burger').on('click', event => {
+    event.preventDefault();
+    const name = $('#burger')
+      .val()
+      .trim();
+    const newBurger = name;
+    $.ajax('/api/burgers', {
+      type: 'POST',
+      data: newBurger
+    }).then(() => {
+      console.log('created new burger');
+      location.reload();
+    });
+  });
+
+  // update burger
+  $('.devour').on('click', function(event) {
+    event.preventDefault();
+    const id = $(this).attr('data-id');
+    const burgerToDevour = {
+      id
+    };
+    console.log(burgerToDevour);
+    $.ajax(
+      `/api/burgers/${id}, {
+      type: 'PUT',
+      data: ${burgerToDevour}
+    }`
+    ).then(() => {
+      console.log('successful');
+    });
+  });
+});
