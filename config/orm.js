695054bac83e5f4faacc16f3fd7056c578d6b033
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-27 14:41:16 -0700

    updated orm statements

--- config/orm.js
+++ config/orm.js
@@ -1,26 +1,53 @@
-const connection = require('./connection');
+// Import the MySQL connection object
+const connection = require('./connection.js');
+
+// Helper function for generating MySQL syntax
+function printQuestionMarks(num) {
+  const array = [];
+
+  for (let i = 0; i < num; i++) {
+    array.push('?');
+  }
+
+  return array.toString();
+}
+
+// Helper function for generating My SQL syntax
+function objToSql(object) {
+  const array = [];
+
+  for (const key in object) {
+    array.push(`${key}=${object[key]}`);
+  }
+
+  return array.toString();
+}
+
+// Create the ORM object to perform SQL queries
 
 const orm = {
   selectAll: (table, callBack) => {
-    const query = `SELECT * FROM burgers;`;
+    const query = `SELECT * FROM ${table};`;
     connection.query(query, (error, result) => {
       if (error) throw error;
       callBack(result);
     });
   },
-  insertOne: data => {
-    const insertQuery = 'INSERT INTO burgers VALUES ?;';
-    console.log(`This is the result: ${data}`);
-    connection.query(insertQuery, [data], (error, result) => {
+  insertOne: (table, columns, values, callBack) => {
+    const insertQuery = `INSERT INTO ${table} (${columns.toString()}) VALUES (${printQuestionMarks(
+      values.length
+    )});`;
+    connection.query(insertQuery, values, (error, result) => {
       if (error) throw error;
+      callBack(result);
       console.log(result, insertQuery);
     });
   },
-  updateOne: (colName, id) => {
-    const updateQuery = 'UPDATE burgers SET ? WHERE ?';
-    console.log(`this is the ${updateQuery}`);
-    connection.query(updateQuery, [colName, id], (error, result) => {
+  updateOne: (table, burgerColumnValues, condition, callBack) => {
+    const updateQuery = `UPDATE ${table} SET ${objToSql(burgerColumnValues)} WHERE ${condition}`;
+    connection.query(updateQuery, (error, result) => {
       if (error) throw error;
+      callBack(result);
       console.log(result);
     });
   }
