e5a449358599991af867ed19a704bca31a25afad
nevermindthelabel <nevermindthelabel@gmail.com>
2019-01-27 14:42:25 -0700

    updated burger model

--- models/burger.js
+++ models/burger.js
@@ -3,12 +3,16 @@ const orm = require('../config/orm');
 const burger = {
   all: callback => orm.selectAll('burgers', response => callback(response)),
 
-  createOne: (values, callback) => {
-    orm.insertOne('burgers', values, response => callback(response));
+  createOne: (columns, values, callBack) => {
+    orm.insertOne('burgers', columns, values, response => {
+      callBack(response);
+    });
   },
 
-  updateOne: (colName, id, callback) => {
-    orm.updateOne('burgers', [colName, id], response => callback(response));
+  updateOne: (burgerColumnValues, condition, callBack) => {
+    orm.updateOne('burgers', burgerColumnValues, condition, response => {
+      callBack(response);
+    });
   }
 };
 
